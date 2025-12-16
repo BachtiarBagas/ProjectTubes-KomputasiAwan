0fba2c807837b2d25caf6be64cdf489cf672d01a
joepetrraa <joepetrraa@gmail.com>
2025-12-16 18:56:42 +0700

    Delete project test directory

--- project test/midtrans_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__) . '/vendor/autoload.php';
-
-// Set Midtrans Configuration
-\Midtrans\Config::$serverKey = 'Mid-server-Emdteo_SpXaiwq00oIeYKFxt';
-\Midtrans\Config::$isProduction = false; // Set false untuk sandbox/testing
-\Midtrans\Config::$isSanitized = true;
-\Midtrans\Config::$is3ds = true;
-?>
