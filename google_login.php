51f86559cd1b067b0a5a874fc31f9909e83b8410
Bachtiar <fathonibachtiar@gmail.com>
2026-01-09 21:38:34 +0700

    perbaikan

--- google_login.php
+++ google_login.php
@@ -5,7 +5,6 @@ session_start();
 
 // ============ KONFIGURASI ANDA (Sudah Benar) ============
 $clientID = '610900154866-i2u0cmiclf1d1132a3ag2kjd5ptm30d2.apps.googleusercontent.com';
-$clientSecret = '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';
 $redirectURI = 'https://foodsite.azurewebsites.net/google_callback.php';
 
 // ========================================================
@@ -17,7 +16,7 @@ $_SESSION['oauth_state'] = $state;
 
 // Buat Google Auth URL
 $auth_url = 'https://accounts.google.com/o/oauth2/v2/auth?' . http_build_query([
-    'client_id'     => $client_id,
+    'client_id'     => $clientID,
     'redirect_uri'  => $redirect_uri,
     'response_type' => 'code',
     'scope'         => $scope,
